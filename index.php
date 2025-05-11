096a229e973652d1d8c4e36cba5bfd0af4e4d500
Pedro Castro <vieiracastropedro7@gmail.com>
2025-05-11 14:49:01 -0300

    serviços subindo com Docker

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto de extensão</title>
+</head>
+<body>
+    <h1>Teste de PHP e Apache rodando com Docker</h1>
+
+    <?php echo "este site está rodando com Docker"; ?>
+    <?php
+        phpinfo();
+    ?>
+</body>
+</html>
