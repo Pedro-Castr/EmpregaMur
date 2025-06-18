03deb5ed563f64580a47caa47629123bad9af51d
Pedro Castro <vieiracastropedro7@gmail.com>
2025-06-18 13:20:43 -0300

    Atualizações do atomphp

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -6,6 +6,12 @@ defined('DEFAULT_CONTROLLER') || define("DEFAULT_CONTROLLER", 'Home');
 // Método padrão
 defined('DEFAULT_METODO') || define("DEFAULT_METODO", 'index');
 
+// Controller autorizados a executar sem login
+defined('CONTROLLER_AUTH') || define('CONTROLLER_AUTH', [
+    "Home",
+    "Login"
+]);
+
 // Definir o time_zone_default
 defined("DEFAULT_TIME_ZONE") || define("DEFAULT_TIME_ZONE", "America/Sao_Paulo");
 
@@ -14,20 +20,20 @@ defined('FILE_MAXSIZE') || define('FILE_MAXSIZE', 5);
 
 // Arquivos aceitos em Uploads
 defined('FILE_ALLOWEDTYPES') || define('FILE_ALLOWEDTYPES', [
-    'image/jpg', 
-    'image/jpeg', 
-    'image/png', 
-    'image/gif', 
+    'image/jpg',
+    'image/jpeg',
+    'image/png',
+    'image/gif',
     'image/bmp',
     'image/webp',
     'image/svg+xml',
     'application/pdf',
     'application/msword',                                                           // DOC (Word 97-2003)
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',      // DOCX 
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document',      // DOCX
     'application/vnd.ms-excel',                                                     // XLS (Excel 97-2003)
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',            // XLSX (Excel 2007+)
     'application/vnd.ms-powerpoint',                                                // PPT (PowerPoint 97-2003)
-    'application/vnd.openxmlformats-officedocument.presentationml.presentation',    // PPTX 
+    'application/vnd.openxmlformats-officedocument.presentationml.presentation',    // PPTX
     'text/plain',                                                                   // TXT
     'text/csv',
     'application/zip',
