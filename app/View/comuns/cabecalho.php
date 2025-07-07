12c78073aa2479797d03fcfd09f5da0221f170c3
Pedro Castro <vieiracastropedro7@gmail.com>
2025-07-07 18:54:33 -0300

    Botão de login no cabeçalho implementado

--- app/View/comuns/cabecalho.php
+++ app/View/comuns/cabecalho.php
@@ -1,3 +1,5 @@
+<?php use Core\Library\Session; ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -10,35 +12,39 @@
 </head>
 <body>
   <header class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-    <div class="container">
-      <a class="d-flex align-items-center" href="<?= baseUrl()?>">
-        <img src="<?= baseUrl() ?>assets/img/logo6.png" alt="Logo" height="100vh" class="me-2">
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
-        aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" href="<?= baseUrl()?>">🏠 Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="<?= baseUrl() ?>Emprego">💼 Empregos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="#">🔔 Notificações</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" href="<?= baseUrl() ?>Login">👤 Perfil</a>
-          </li>
-        </ul>
-      </div>
+  <div class="container">
+    <a class="navbar-brand d-flex align-items-center" href="<?= baseUrl() ?>">
+      <img src="<?= baseUrl() ?>assets/img/logo6.png" alt="Logo" height="100vh" class="me-2">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+      aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" href="<?= baseUrl()?>">🏠 Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="<?= baseUrl() ?>Emprego">💼 Empregos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">🔔 Notificações</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">👤 Perfil</a>
+        </li>
+      </ul>
     </div>
-  </header>
+    <?php if (!Session::get("userId")): ?>
+      <a href="<?= baseUrl() ?>Login" class="btn btn-primary ms-2">Faça login</a>
+    <?php endif; ?>
+  </div>
+</header>
 
   <!-- Bootstrap JS (com Popper) -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
